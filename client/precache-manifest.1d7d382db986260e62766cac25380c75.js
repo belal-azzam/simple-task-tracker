343dce186fba1812e29850b9f9e935b7c91b4457
= <=>
2019-08-26 22:28:02 +0200

    add clinet code

---
+++ client/precache-manifest.1d7d382db986260e62766cac25380c75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfb47ad79bdd9b67645cc1fabf2d6128",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d7f28644a14135c6d87",
+    "url": "/static/css/2.03688171.chunk.css"
+  },
+  {
+    "revision": "52e1ffc0d377c369d61d",
+    "url": "/static/css/main.fc7a9dfb.chunk.css"
+  },
+  {
+    "revision": "0d7f28644a14135c6d87",
+    "url": "/static/js/2.7535d641.chunk.js"
+  },
+  {
+    "revision": "52e1ffc0d377c369d61d",
+    "url": "/static/js/main.f5c750e1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "acdf48026701b8cf70d39e1b392d3a7f",
+    "url": "/static/media/login-user.acdf4802.svg"
+  }
+]);
